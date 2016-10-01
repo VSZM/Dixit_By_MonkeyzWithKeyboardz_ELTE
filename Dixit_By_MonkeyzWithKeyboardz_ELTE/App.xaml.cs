eac43b7f698bf69d9f4324ecd0a335f82e019f2b
Szegedi Gabor <wayasam@gmail.com>
2016-10-01 18:14:33 +0200

    Initial wpf project added

---
+++ Dixit_By_MonkeyzWithKeyboardz_ELTE/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dixit_By_MonkeyzWithKeyboardz_ELTE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
