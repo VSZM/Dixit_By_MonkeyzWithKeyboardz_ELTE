fd1682617d74b5f9129739fc09c900c08baa7149
Szeligor <kzsofia1992@gmail.com>
2016-10-18 05:04:39 +0200

    Logic -> Service

--- Dixit_Service/Interfaces/IDixitServiceCallback.cs
+++ Dixit_Service/Interfaces/IDixitServiceCallback.cs
@@ -1,31 +1,30 @@
-﻿using Dixit_Service.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using Dixit_Logic.Interfaces;
 
 namespace Dixit_Service
 {
-    [ServiceContract]
     public interface IDixitServiceCallback
     {
         /// <summary>
         /// Jelzes a klienseknek, hogy a jatek elindult.
         /// </summary>
         [OperationContract(IsOneWay = true)]
-        void GameStart(GameState state);
+        void GameStart(IGameState state);
         /// <summary>
         /// Jelzes a klienseknek, hogy jatek allapota megvaltozott.
         /// </summary> 
         /// <param name="player">A soron kovetkezo jatekos.</param>
         [OperationContract(IsOneWay = true)]
-        void GameStateChanged(GameState state);
+        void GameStateChanged(IGameState state);
         /// <summary>
         /// Jelzes a klienseknek, hogy a jatek vegetert.
         /// </summary>
         [OperationContract(IsOneWay = true)]
-        void GameEnd(GameState state);
+        void GameEnd(IGameState state);
     }
 }
