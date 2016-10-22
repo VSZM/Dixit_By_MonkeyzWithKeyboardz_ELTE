617b25784ab246fba22b096826a0dbe65977ffb0
ballaibrigitta42 <ballaibrigitta42@gmail.com>
2016-10-22 09:34:24 +0200

    DesignPersistenceLayer
    Add IImageAcces interface and a dll file which contains the images for
    the cards.

---
+++ Dixit_Logic/Interfaces/IImageAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Dixit_Logic.Interfaces
+{
+    /// <summary>
+    /// This interface loads the images for the cards.
+    /// </summary>
+    interface IImageAccess
+    {
+        /// <summary>
+        /// Get the ids of images in a list.
+        /// </summary>
+        /// <returns>ids of images</returns>
+        List<int> GetIDList();
+
+        /// <summary>
+        /// Get the image by its id.
+        /// </summary>
+        /// <param name="id">id of an image</param>
+        /// <returns>image</returns>
+        Bitmap GetImageById(int id);       
+    }
+}
