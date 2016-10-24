5c2356f133294b51b8e0b17801d9f23cb145dbc2
selleim <sellei_m@hotmail.com>
2016-10-24 19:25:12 +0200

    Login/Logout

--- Dixit_Service/DixitService.svc.cs
+++ Dixit_Service/DixitService.svc.cs
@@ -12,35 +12,50 @@ using System.Reflection;
 namespace Dixit_Service
 {
     [ServiceKnownType("RegisterKnownTypes", typeof(DixitService))]
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Single)]
     public class DixitService : IDixitService
     {
+        private Dictionary<string, UserInfo> Users = new Dictionary<string, UserInfo>();
+        //private Dictionary<string, IDixitGame> Games = new Dictionary<string, IDixitGame>();
+        private IDixitGame Game;
+
+        #region Login methods
+        public void Login()
+        {
+            var ui = new UserInfo();
+            ui.Username = OperationContext.Current.ServiceSecurityContext.PrimaryIdentity.Name;
+            ui.Callback = OperationContext.Current.GetCallbackChannel<IDixitServiceCallback>();
+            Users.Add(ui.Username, ui);
+        }
+        public void Logout()
+        {
+            RemoveUser(OperationContext.Current.ServiceSecurityContext.PrimaryIdentity.Name);
+        }
+        private void RemoveUser(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username)) { return; }
+
+            if (Users.ContainsKey(username))
+            {
+                Users.Remove(username);
+            }
+        }
+        #endregion
         public CreateGameResult CreateGame(string name)
         {
-            throw new NotImplementedException();
+            var r = new CreateGameResult();
+            //r.Games = 
+            return r;
         }
-
         public JoinGameResult JoinGame(IDixitGame game)
         {
             throw new NotImplementedException();
         }
-
         public void LeaveGame(IDixitGame game)
         {
             throw new NotImplementedException();
         }
-
         public List<IDixitGame> ListGames()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Login()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Logout()
         {
             throw new NotImplementedException();
         }
@@ -49,12 +64,10 @@ namespace Dixit_Service
         {
             throw new NotImplementedException();
         }
-
         public SelectCardResult SelectCardWithStory(IDixitGame game, ICard card, string story)
         {
             throw new NotImplementedException();
         }
-
         public static IEnumerable<Type> RegisterKnownTypes(ICustomAttributeProvider provider)
         {
             return Dixit_Logic.KnownTypesProvider.GetKnownTypes();
---
+++ Dixit_Service/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using Dixit_ServiceLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dixit_Service
+{
+    public class UserInfo
+    {
+        public string Username;
+        public IDixitServiceCallback Callback;
+    }
+}
