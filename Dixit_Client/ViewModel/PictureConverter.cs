5c3ab9c940d21d392a86c20735a9d5898cbdfc50
SilverGeri <angeli.gergely@gmail.com>
2016-11-15 09:10:21 +0100

    add PictureConverter

---
+++ Dixit_Client/ViewModel/PictureConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+using Dixit_Data;
+
+namespace Dixit_Client.ViewModel
+{
+    /// <summary>
+    /// Gets the picture's Id and returns the corresponding image
+    /// </summary>
+    class PictureConverter : IValueConverter
+    {
+        static CardAccess ca = new CardAccess();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Binding.DoNothing;
+            }
+            
+            return ca.GetImageById((int)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
